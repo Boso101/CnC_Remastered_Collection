ca94efa120e8144d249591212b8421ec053bb230
Boso101 <bosorevived@gmail.com>
2021-07-11 22:07:21 +0930

    Update DLLInterface.cpp

--- REDALERT/DLLInterface.cpp
+++ REDALERT/DLLInterface.cpp
@@ -1107,10 +1107,10 @@ void GlyphX_Assign_Houses(void)
 		if (!housep->IsHuman) {
 			housep->IsStarted = true;
 			housep->IsAlerted = true; // Enable AutoCreate
-			housep->IsBaseBuilding = true;
+			//housep->IsBaseBuilding = true;
 			strncpy(housep->IniName, Text_String(TXT_COMPUTER), HOUSE_NAME_MAX);
-			//housep->IQ = Rule.MaxIQ;
-			housep->IQ = 3;
+			housep->IQ = Rule.MaxIQ;
+		
 			//housep->Control.TechLevel = _build_tech[BuildLevel];
 		} else {
 			housep->IQ = 0;
